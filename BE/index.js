81093d04f239eb5a78f4585b32a3008f5bae65fe
Ankan1998 <ankanroni3@gmail.com>
2021-12-17 19:15:06 +0530

    router linked to index

--- BE/index.js
+++ BE/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const UserRouter = require('./router/user')
 
 const app = express()
-const port = process.nextTick.PORT || 3000
+const port = process.env.PORT || 3000
 
 app.use(express.json())
+
+app.use(UserRouter)
 
 app.listen(port,()=>{
     console.log('Server listening at Port: ' + port)
