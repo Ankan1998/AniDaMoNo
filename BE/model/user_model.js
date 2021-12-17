1ebd141c7ecc6bccd10f87bbeb2bae522cd91e7f
Ankan1998 <ankanroni3@gmail.com>
2021-12-17 19:14:47 +0530

    user model added

---
+++ BE/model/user_model.js
@@ -0,0 +1,85 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        validate(val) {
+            if (!validator.isEmail(val)) {
+                throw new Error('Email invalid')
+            }
+        }
+    },
+    password: {
+        type: String
+    },
+    dp:{
+        type: Buffer
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true,
+        }
+    }]
+
+}, {
+    timestamps: true
+})
+
+// Hiding password and token
+UserSchema.methods.toJSON = function () {
+    const Obj = this.toObject()
+    delete Obj.password
+    delete Obj.tokens
+    return Obj
+}
+
+//Json web token instance method
+UserSchema.methods.generateToken = async function (id) {
+    const token = jwt.sign({
+        _id: id.toString()
+    }, "ankan12345")
+    this.tokens.push({
+        token: token
+    })
+    await this.save()
+    return token
+}
+
+// Match credential static method(class method)
+UserSchema.statics.matchCred = async function (emailID, password) {
+    const user = await users.findOne({
+        email: emailID
+    })
+    if (!user) {
+        throw new Error("Login Error")
+    }
+    const isSame = await bcrypt.compare(password, user.password)
+    if (!isSame) {
+        throw new Error("Login Error")
+    }
+    return user
+
+}
+
+
+UserSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 8)
+    }
+    next()
+})
+
+
+const users = mongoose.model('User', UserSchema)
+
+module.exports = users
