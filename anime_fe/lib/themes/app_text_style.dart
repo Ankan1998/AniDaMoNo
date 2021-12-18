b7f513d75717c8bde8b102f81c842b0af93a21ac
Ankan1998 <ankanroni3@gmail.com>
2021-12-18 15:01:16 +0530

    app text style started

---
+++ anime_fe/lib/themes/app_text_style.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'package:google_fonts/google_fonts.dart';
+
+class TextStyles{
+  TextStyles._();
+
+  static TextStyle largeTitle = TextStyle(
+    fontFamily: GoogleFonts.poppins().fontFamily,
+    fontWeight: FontWeight.w100,
+    fontSize: 42.0
+  );
+
+}
