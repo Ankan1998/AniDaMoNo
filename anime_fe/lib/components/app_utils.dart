73e67c207582e45b377d7de65dda49d6c3322cf6
Ankan1998 <ankanroni3@gmail.com>
2021-12-18 20:07:20 +0530

    validation function for name added

--- anime_fe/lib/components/app_utils.dart
+++ anime_fe/lib/components/app_utils.dart
@@ -6,4 +6,10 @@ class AppUtils{
         r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
         .hasMatch(text);
   }
+
+  static bool isValidName(String text) {
+    return RegExp(
+        r'^([a-zA-Z ]+)$')
+        .hasMatch(text);
+  }
 }
