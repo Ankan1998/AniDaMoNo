98d5fc58203656c758f54538f34caa86a7041c85
Ankan1998 <ankanroni3@gmail.com>
2021-12-24 20:56:07 +0530

    repo with verify login added

---
+++ anime_fe/lib/components/repo.dart
@@ -0,0 +1,16 @@
+import 'package:anime_fe/components/data_provider.dart';
+import 'package:anime_fe/modules/login/model/login_model.dart';
+import 'package:dio/dio.dart';
+
+class Repository{
+  DataProvider _dataProvider = DataProvider();
+
+  Future<int> verifyLogin(LoginModel loginData) async {
+    var result = await _dataProvider.requestLoginEndpoint(loginData.email,loginData.password);
+    if(result.statusCode){
+      return result.statusCode;
+    }
+    return 503;
+
+  }
+}
