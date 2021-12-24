2aee90200fdd415c8237cf2f9dedeb9b7a1caf6e
Ankan1998 <ankanroni3@gmail.com>
2021-12-24 13:12:07 +0530

    login and signup model added

---
+++ anime_fe/lib/modules/login/model/login_model.dart
@@ -0,0 +1,48 @@
+class LoginModel {
+  User user;
+  String token;
+
+  LoginModel({this.user, this.token});
+
+  LoginModel.fromJson(Map<String, dynamic> json) {
+    user = json['user'] != null ? new User.fromJson(json['user']) : null;
+    token = json['token'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.user != null) {
+      data['user'] = this.user.toJson();
+    }
+    data['token'] = this.token;
+    return data;
+  }
+}
+
+class User {
+  String sId;
+  String name;
+  String email;
+  String createdAt;
+  String updatedAt;
+
+  User({this.sId, this.name, this.email, this.createdAt, this.updatedAt});
+
+  User.fromJson(Map<String, dynamic> json) {
+    sId = json['_id'];
+    name = json['name'];
+    email = json['email'];
+    createdAt = json['createdAt'];
+    updatedAt = json['updatedAt'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['_id'] = this.sId;
+    data['name'] = this.name;
+    data['email'] = this.email;
+    data['createdAt'] = this.createdAt;
+    data['updatedAt'] = this.updatedAt;
+    return data;
+  }
+}
