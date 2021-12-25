cf80da48dfbdc57558b5a055415df749ee2c7151
Ankan1998 <ankanroni3@gmail.com>
2021-12-25 14:45:15 +0530

    MultiBlocProvider added over MaterialApp

--- anime_fe/lib/main.dart
+++ anime_fe/lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:anime_fe/modules/login/bloc/login_bloc.dart';
 import 'package:anime_fe/modules/login/ui/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,15 +11,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Anime App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+            create: (context) => LoginBloc()
+        )
+      ],
+      child: MaterialApp(
+        title: 'Anime App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: SplashScreen(),
       ),
-      home: SplashScreen(),
     );
   }
 }
-
-
 
