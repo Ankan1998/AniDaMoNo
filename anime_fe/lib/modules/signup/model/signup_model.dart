47249b59317c66ee0759c050c1932c54925a9fa9
Ankan1998 <ankanroni3@gmail.com>
2021-12-25 15:58:25 +0530

    signup model added

--- anime_fe/lib/modules/signup/model/signup_model.dart
+++ anime_fe/lib/modules/signup/model/signup_model.dart
@@ -1,48 +1,21 @@
-class SignupModel {
-  User user;
-  String token;
-
-  SignupModel({this.user, this.token});
-
-  SignupModel.fromJson(Map<String, dynamic> json) {
-    user = json['user'] != null ? new User.fromJson(json['user']) : null;
-    token = json['token'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    if (this.user != null) {
-      data['user'] = this.user.toJson();
-    }
-    data['token'] = this.token;
-    return data;
-  }
-}
-
-class User {
+class SignUpModel {
   String name;
   String email;
-  String sId;
-  String createdAt;
-  String updatedAt;
+  String password;
 
-  User({this.name, this.email, this.sId, this.createdAt, this.updatedAt});
+  SignUpModel({this.name, this.email, this.password});
 
-  User.fromJson(Map<String, dynamic> json) {
+  SignUpModel.fromJson(Map<String, dynamic> json) {
     name = json['name'];
     email = json['email'];
-    sId = json['_id'];
-    createdAt = json['createdAt'];
-    updatedAt = json['updatedAt'];
+    password = json['password'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['name'] = this.name;
     data['email'] = this.email;
-    data['_id'] = this.sId;
-    data['createdAt'] = this.createdAt;
-    data['updatedAt'] = this.updatedAt;
+    data['password'] = this.password;
     return data;
   }
 }
