2a8a63351c4064333830cbe0a1ee9a7ba5483ac3
Ankan1998 <ankanroni3@gmail.com>
2022-01-08 23:49:30 +0530

    index updated according to mergd schema

--- BE/index.js
+++ BE/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const UserRouter = require('./router/rest/user')
 const {graphqlHTTP} = require('express-graphql')
-const graphQlSchema = require('./router/graphql/schemas/merged_schemas')
+const { buildSchema, print } = require('graphql');
 const graphQlResolvers = require('./router/graphql/resolvers/main_resolver')
+const typeDefs = require('./router/graphql/schemas/merged_schemas');
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -10,10 +11,11 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 
 app.use(UserRouter)
+const schemaMerged = buildSchema(print(typeDefs));
 app.use(
     '/graphql',
     graphqlHTTP({
-      schema: graphQlSchema,
+      schema: schemaMerged,
       rootValue: graphQlResolvers,
       graphiql: true
     })
