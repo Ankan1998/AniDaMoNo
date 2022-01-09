796020cb3c615b2a5fbd3ec2c253c0fb6d324d2c
Ankan1998 <ankanroni3@gmail.com>
2022-01-09 21:29:44 +0530

    merged schema updated with character detail

--- BE/router/graphql/schemas/merged_schemas.js
+++ BE/router/graphql/schemas/merged_schemas.js
@@ -1,10 +1,12 @@
 const { buildSchema, print } = require('graphql');
 const { mergeTypeDefs } = require('@graphql-tools/merge')
-const characterSearch = require('./searchCharacter_schema');
+const characterSearch = require('./character/searchCharacter_schema');
+const characterDetail = require('./character/character_Detail_schema.js')
 
 const rootTypes = `
 type RootQuery {
     characters: [Character]
+    characterById: CharacterDetail
 }
 
 schema {
@@ -12,7 +14,7 @@ schema {
 }
 `;
 
-const types = [characterSearch, rootTypes];
+const types = [characterSearch, characterDetail,rootTypes];
 const typeDefs = mergeTypeDefs(types)
 const schemaMerged = buildSchema(print(typeDefs));
 
