58993a2ee7712e792c2dbabadefea7413b7f8e29
Ankan1998 <ankanroni3@gmail.com>
2022-01-10 21:23:59 +0530

    character detail resolver with main resolver

--- BE/router/graphql/resolvers/main_resolver.js
+++ BE/router/graphql/resolvers/main_resolver.js
@@ -1,7 +1,9 @@
 const characterResolver = require('./character/searchCharacter_resolver')
+const characterDetail = require('./character/characterDetail_resolver')
 
 const RootResolver = {
     ...characterResolver,
+    ...characterDetail
     
 }
 
