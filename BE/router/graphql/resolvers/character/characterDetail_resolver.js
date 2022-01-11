083123fbe072e9f9e66d490c57414ed2192191d9
Ankan1998 <ankanroni3@gmail.com>
2022-01-11 10:56:19 +0530

    charcter detail resolver added

--- BE/router/graphql/resolvers/character/characterDetail_resolver.js
+++ BE/router/graphql/resolvers/character/characterDetail_resolver.js
@@ -1,4 +1,5 @@
 const axios = require('axios')
+const { response } = require('express')
 
 const characterDetail = {
     characterById: async () => {
@@ -7,16 +8,39 @@ const characterDetail = {
             'https://api.jikan.moe/v4/characters/17/anime',
             'https://api.jikan.moe/v4/characters/17/voices'
         ]
-        await axios.all(endpoints.map((endpoint) => axios.get(endpoint))).then(
-            axios.spread((a, b, c) => {
-                return [a.data, b.data, c.data]
+        const response_map = await Promise.all(endpoints.map((endpoint) => axios.get(endpoint))).then(
+            axios.spread((character, anime, voice) => {
+                return [character.data, anime.data, voice.data]
             })
-        ).then((res) => console.log({
-            "a": res[0].data,
-            "b": res[1].data,
-            "c": res[2].data
-        }))
-        return 0;
+        ).then((res) => {
+            return {
+                "character": res[0].data,
+                "anime": res[1].data,
+                "voices": res[2].data
+            }
+        })
+        const appearence_list = []
+        const voice_actor_list = []
+        response_map.anime.forEach(element => {
+            appearence_list.push({
+                "role":element.role,
+                "anime_id": element.anime.mal_id,
+                "anime_title":element.anime.title
+            })
+            
+        })
+        response_map.voices.forEach(element => {
+            voice_actor_list.push({
+                "language":element.language,
+                "actor_id": element.person.mal_id,
+                "name":element.person.name
+            })
+            
+        })
+        
+        response_map.character["appeareance"] = appearence_list
+        response_map.character["voice_actor"] = voice_actor_list
+        return response_map.character;
 
     }
 }
