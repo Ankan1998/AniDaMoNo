1e6db728f8eeeb0f96dcea380a0225192f7a91bf
Ankan1998 <ankanroni3@gmail.com>
2022-01-12 20:54:38 +0530

    merged schema updated with anime detail

--- BE/router/graphql/schemas/merged_schemas.js
+++ BE/router/graphql/schemas/merged_schemas.js
@@ -3,12 +3,14 @@ const { mergeTypeDefs } = require('@graphql-tools/merge')
 const characterSearch = require('./character/searchCharacter_schema');
 const characterDetail = require('./character/character_Detail_schema.js')
 const animeSearch = require('./anime/search_anime_schema')
+const animeDetail = require('./anime/anime_detail_schema')
 
 const rootTypes = `
 type RootQuery {
     characters(name:String!): [Character]
     characterById(id:Int!): CharacterDetail
-    animes(name:String!):[Animes]
+    animes(name:String!): [Animes]
+    animeById(id:Int!): AnimeDetail
 }
 
 schema {
@@ -16,7 +18,7 @@ schema {
 }
 `;
 
-const types = [characterSearch, characterDetail,animeSearch,rootTypes];
+const types = [characterSearch, characterDetail,animeSearch,animeDetail,rootTypes];
 const typeDefs = mergeTypeDefs(types)
 const schemaMerged = buildSchema(print(typeDefs));
 
