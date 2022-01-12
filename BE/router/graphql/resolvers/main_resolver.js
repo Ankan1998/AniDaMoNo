8cf573fa9f76eba8a5e63c573714017b4275bd8c
Ankan1998 <ankanroni3@gmail.com>
2022-01-12 21:16:41 +0530

    main resolver updated

--- BE/router/graphql/resolvers/main_resolver.js
+++ BE/router/graphql/resolvers/main_resolver.js
@@ -1,11 +1,13 @@
 const characterSearchResolver = require('./character/searchCharacter_resolver')
 const characterDetailResolver = require('./character/characterDetail_resolver')
 const animeSearchResolver = require("./anime/search_anime_resolver")
+const animeDetailResolver = require('./anime/anime_detail_resolver')
 
 const RootResolver = {
     ...characterSearchResolver,
     ...characterDetailResolver,
-    ...animeSearchResolver
+    ...animeSearchResolver,
+    ...animeDetailResolver
     
 }
 
