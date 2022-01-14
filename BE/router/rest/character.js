5f4727b683031559043221738c787e4c93f75965
Ankan1998 <ankanroni3@gmail.com>
2022-01-14 18:36:03 +0530

    create, delete, read for characters added

--- BE/router/rest/character.js
+++ BE/router/rest/character.js
@@ -18,4 +18,40 @@ router.post('/characters/fav/:id', auth, async(req, res)=>{
     }
 })
 
+// get Characters according to User
+router.get('/characters', auth, async (req, res) => {
+    try {
+        await req.user.populate({
+            path: 'character_virtuals',
+        })
+        res.status(200).send(req.user.character_virtuals)
+
+    } catch (e) {
+        res.status(400).send()
+    }
+})
+
+// Delete an Character
+router.delete('/characters/delete/:id', auth,async (req, res) => {
+    try {
+        const delCount = await Character.deleteOne({_id:req.params.id,owner:req.user._id})
+        if(delCount.deletedCount===0){
+            return res.status(400).send()
+        }
+        res.status(200).send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+// Delete all Characters
+router.delete('/characters/deleteAll', auth, async (req, res) => {
+    try {
+        await Character.deleteMany({owner:req.user._id})
+        res.status(200).send()
+
+    } catch (e) {
+        res.status(400).send()
+    }
+})
 module.exports = router
