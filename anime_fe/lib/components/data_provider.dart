40668b03aa6a1e40ebb814b9390968d925b3c6fe
Ankan1998 <ankanroni3@gmail.com>
2022-01-14 19:52:14 +0530

    created a function to set dio value

--- anime_fe/lib/components/data_provider.dart
+++ anime_fe/lib/components/data_provider.dart
@@ -1,10 +1,19 @@
+import 'package:anime_fe/modules/home/model/user_info.dart';
 import 'package:dio/dio.dart';
 
 class DataProvider {
-  Dio dio = new Dio();
   String baseUrl = 'http://192.168.8.116:3000';
 
+  Dio getDio() {
+    Dio dio = new Dio();
+    UserInfo _userInfo = UserInfo();
+    dio.options.headers["authorization"] = "Bearer ${_userInfo.getAuthToken()}";
+    return dio;
+  }
+
   Future<dynamic> requestLoginEndpoint(String email, String password) async {
+    Dio dio = getDio();
+    print(dio.options.headers["authorization"]);
     try{
       Response response = await dio.post(
         baseUrl+'/users/login',
@@ -21,6 +30,7 @@ class DataProvider {
   }
 
   Future<dynamic> requestSignupEndpoint(String name, String email, String password) async {
+    Dio dio = getDio();
     try{
       Response response = await dio.post(
           baseUrl+'/users/signup',
@@ -37,8 +47,8 @@ class DataProvider {
     }
   }
 
-  Future<dynamic> requestProfileEndpoint(String token) async {
-    dio.options.headers["Authorization"] = "Bearer ${token}";
+  Future<dynamic> requestProfileEndpoint() async {
+    Dio dio = getDio();
     try{
       Response response = await dio.get(
           baseUrl+'/users/me',
