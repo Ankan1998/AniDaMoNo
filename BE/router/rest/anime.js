a3e38a013812bc74f03c0e8cf96b08e1c08c63c0
Ankan1998 <ankanroni3@gmail.com>
2022-01-14 18:05:15 +0530

    create, read, delete of anime endpoint added

--- BE/router/rest/anime.js
+++ BE/router/rest/anime.js
@@ -18,4 +18,41 @@ router.post('/animes/save/:id', auth, async(req, res)=>{
     }
 })
 
+// get anime according to User
+router.get('/animes', auth, async (req, res) => {
+    try {
+        await req.user.populate({
+            path: 'anime_virtuals',
+        })
+        res.status(200).send(req.user.anime_virtuals)
+
+    } catch (e) {
+        res.status(400).send()
+    }
+})
+
+// Delete an anime
+router.delete('/animes/delete/:id', auth,async (req, res) => {
+    try {
+        const delCount = await Anime.deleteOne({_id:req.params.id,owner:req.user._id})
+        if(delCount.deletedCount===0){
+            return res.status(400).send()
+        }
+        res.status(200).send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+// Delete all Task
+router.delete('/animes/deleteAll', auth, async (req, res) => {
+    try {
+        await Anime.deleteMany({owner:req.user._id})
+        res.status(200).send()
+
+    } catch (e) {
+        res.status(400).send()
+    }
+})
+
 module.exports = router
