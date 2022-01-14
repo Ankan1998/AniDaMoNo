9db5e8d89cbf87a90d3773b283a9a2f60d015911
Ankan1998 <ankanroni3@gmail.com>
2022-01-14 20:58:44 +0530

    data provider all USER route endpoints added

--- anime_fe/lib/components/data_provider.dart
+++ anime_fe/lib/components/data_provider.dart
@@ -11,9 +11,9 @@ class DataProvider {
     return dio;
   }
 
+  // USERS
   Future<dynamic> requestLoginEndpoint(String email, String password) async {
     Dio dio = getDio();
-    print(dio.options.headers["authorization"]);
     try{
       Response response = await dio.post(
         baseUrl+'/users/login',
@@ -52,6 +52,91 @@ class DataProvider {
     try{
       Response response = await dio.get(
           baseUrl+'/users/me',
+      );
+      return response;
+
+    } catch(e) {
+      return e;
+    }
+  }
+
+  Future<dynamic> requestUpdateUserEndpoint(String name, String password) async {
+    Dio dio = getDio();
+    try{
+      Response response = await dio.post(
+          baseUrl+'/users/update',
+          data: {
+            "name":name,
+            "password":password
+          }
+      );
+      return response;
+
+    } catch(e) {
+      return e;
+    }
+  }
+
+  Future<dynamic> requestLogoutEndpoint() async {
+    Dio dio = getDio();
+    try{
+      Response response = await dio.get(
+        baseUrl+'/users/logout',
+      );
+      return response;
+
+    } catch(e) {
+      return e;
+    }
+  }
+
+  Future<dynamic> requestLogoutAllEndpoint() async {
+    Dio dio = getDio();
+    try{
+      Response response = await dio.get(
+        baseUrl+'/users/logoutAll',
+      );
+      return response;
+
+    } catch(e) {
+      return e;
+    }
+  }
+
+  Future<dynamic> requestDeleteEndpoint() async {
+    Dio dio = getDio();
+    try{
+      Response response = await dio.delete(
+        baseUrl+'/users/delete',
+      );
+      return response;
+
+    } catch(e) {
+      return e;
+    }
+  }
+
+  Future<dynamic> requestUploadDpEndpoint(String url) async {
+    Dio dio = getDio();
+    try{
+      Response response = await dio.post(
+          baseUrl+'/upload/url',
+          data: {
+            "dp":url,
+          }
+      );
+      return response;
+
+    } catch(e) {
+      return e;
+    }
+  }
+
+  Future<dynamic> requestDpEndpoint() async {
+    Dio dio = getDio();
+    try{
+      Response response = await dio.get(
+        baseUrl+'/download/url',
       );
       return response;
 
