3bf55c71385c61e8e5f406e088e3cf51dc2450a8
Ankan1998 <ankanroni3@gmail.com>
2022-02-13 16:58:40 +0530

    Updated with graphql service

--- anime_fe/lib/components/repo.dart
+++ anime_fe/lib/components/repo.dart
@@ -1,14 +1,19 @@
 import 'package:anime_fe/components/data_provider.dart';
+import 'package:anime_fe/components/graphql_services.dart';
 import 'package:anime_fe/modules/home/model/user_info.dart';
 import 'package:anime_fe/modules/home/model/user_model.dart';
 import 'package:anime_fe/modules/login/model/login_model.dart';
 import 'package:anime_fe/modules/signup/model/signup_model.dart';
 import 'package:dio/dio.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Repository{
+
   DataProvider _dataProvider = DataProvider();
   UserInfo _userInfo = UserInfo();
+  GraphqlService _gqlService = GraphqlService();
+
   Future<int> verifyLogin(LoginModel loginData) async {
     final prefs = await SharedPreferences.getInstance();
     var result = await _dataProvider.requestLoginEndpoint(loginData.email,loginData.password);
@@ -37,5 +42,17 @@ class Repository{
     _userInfo.setAuthToken(_userModel.token);
     await prefs.setString('token', _userInfo.getAuthToken());
     return result.statusCode;
+  }
+
+  // GraphQL
+
+  Future<QueryResult> searchAnime(String animeName) async {
+    QueryResult result = await _gqlService.searchAnime(animeName);
+    return result;
+  }
+
+  Future<QueryResult> searchCharacter(String characterName) async {
+    QueryResult result = await _gqlService.searchCharacter(characterName);
+    return result;
   }
 }
