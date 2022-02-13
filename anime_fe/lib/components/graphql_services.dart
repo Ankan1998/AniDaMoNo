45ebd05d4aaba9597befd512bc2be383fc70d42d
Ankan1998 <ankanroni3@gmail.com>
2022-02-13 16:58:13 +0530

    graphql service updated with search character

--- anime_fe/lib/components/graphql_services.dart
+++ anime_fe/lib/components/graphql_services.dart
@@ -3,18 +3,30 @@ import 'package:graphql_flutter/graphql_flutter.dart';
 
 import 'graphql_client.dart';
 
-class GraphqlService{
-  Future<dynamic> searchAnime(
-      String anime_name) async {
+class GraphqlService {
+  Future<dynamic> searchAnime(String anime_name) async {
     QueryResult result = await getGraphqlClient(header: Map()).then(
-          (value) => value.value.query(
+      (value) => value.value.query(
         QueryOptions(
           fetchPolicy: FetchPolicy.networkOnly,
           document: gql(GraphqlQuery.getAnimes(anime_name)),
         ),
       ),
     );
-    print(result.data["animes"]);
-    return 0;
+    return result;
+  }
+
+  Future<dynamic> searchCharacter(String character_name) async {
+    QueryResult result = await getGraphqlClient(header: Map()).then(
+          (value) => value.value.query(
+        QueryOptions(
+          fetchPolicy: FetchPolicy.networkOnly,
+          document: gql(GraphqlQuery.getCharacter(character_name)),
+        ),
+      ),
+    );
+
+    //SearchCharacterModel _searchCharacter = SearchCharacterModel.fromJson(result.data);
+    return result;
   }
 }
